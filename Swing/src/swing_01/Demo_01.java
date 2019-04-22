6d1d9fb9978a2f3ac4e0424b4ce10d1ccb6fca6e
ZhangChen <183751147@qq.com>
2019-04-22 10:32:32 +0800

    解决冲突..

--- Swing/src/swing_01/Demo_01.java
+++ Swing/src/swing_01/Demo_01.java
@@ -5,16 +5,16 @@ import javax.swing.JFrame;
 public class Demo_01 {
 
 	public static void main(String[] args){
-		//555
-		//搭建窗口类
+		//888
+		//鎼缓绐楀彛绫�
 		JFrame jf = new JFrame();
-		//设置大小
+		//璁剧疆澶у皬
 		jf.setSize(400, 400);
-		//设置位置
+		//璁剧疆浣嶇疆
 		jf.setLocation(100, 100);
-		//设置关闭方式
+		//璁剧疆鍏抽棴鏂瑰紡
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//设置窗体显示
+		//璁剧疆绐椾綋鏄剧ず
 		jf.setVisible(true);
 		
 	}
